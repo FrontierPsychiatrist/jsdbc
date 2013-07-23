8b5642291fb58d53d1dfe3b3bc09a47900d27b4d
Moritz Schulze <moritz.schulze@gmail.com>
2013-07-23 23:25:06 +0200

    Removed C cruft

--- src/nativemysql.cc
+++ src/nativemysql.cc
@@ -2,6 +2,7 @@
 
 #include <mysql.h>
 #include <vector>
+#include <string>
 
 #include <iostream>
 using namespace v8;
@@ -9,65 +10,43 @@ using namespace v8;
 MYSQL mysql;
 
 struct Row {
-  char** fieldValues;
+  std::vector<std::string> fieldValues;
 };
 
 struct QueryBaton {
   QueryBaton(Persistent<Function> _callback, char* _query) :
-    request(), callback(_callback), query(_query), rows(0), numRows(0), fieldNames(0), numFields(0),
-    errorText(0) {};
+    request(), callback(_callback), query(_query), rows(), fieldNames(), errorText() {};
   ~QueryBaton() {
-    if(rows != 0) {
-      for(int i = 0; i < numRows; i++) {
-        for(int j = 0; j < numFields; j++) {
-          delete[] rows[i]->fieldValues[j];
-        }
-        delete[] rows[i]->fieldValues;
-        delete rows[i];
-      }
-      delete[] rows;
-    }
-    if(fieldNames != 0) {
-      for(int i = 0; i < numFields; i++) {
-        delete[] fieldNames[i];
-      }
-      delete[] fieldNames;
+    for(unsigned int i = 0; i < rows.size(); i++) {
+      delete rows[i];
     }
   }
   uv_work_t request;
   Persistent<Function> callback;
-  char* query;
-  Row** rows;
-  int numRows;
-  char** fieldNames;
-  int numFields;
-  const char* errorText;
+  std::string query;
+  std::vector<Row*> rows;
+  std::vector<std::string> fieldNames;
+  std::string errorText;
 };
 
 static void query(uv_work_t* req) {
   QueryBaton* baton = static_cast<QueryBaton*>(req->data);
-  int error = mysql_query(&mysql, baton->query);
-  
-  baton->errorText = 0;
+  int error = mysql_query(&mysql, baton->query.c_str());
+
   if(error) {
-    baton->rows = 0;
-    //TODO: copy this
-    baton->errorText = mysql_error(&mysql);
+
+    //baton->errorText = mysql_error(&mysql);
   } else {
     MYSQL_RES* result;
     result = mysql_store_result(&mysql);
     if(result) {
-      baton->numRows = mysql_num_rows(result);
-      baton->rows = new Row*[baton->numRows];
-      baton->numFields = mysql_num_fields(result);
-      baton->fieldNames = new char*[baton->numFields];
+      baton->rows.resize(mysql_num_rows(result));
+      baton->fieldNames.resize(mysql_num_fields(result));
 
       MYSQL_FIELD* mysqlField;
-      unsigned int fieldCounter = 0;
+      
       while((mysqlField = mysql_fetch_field(result))) {
-        baton->fieldNames[fieldCounter] = new char[mysqlField->name_length + 1];
-        memcpy(baton->fieldNames[fieldCounter], mysqlField->name, mysqlField->name_length + 1 );
-        fieldCounter++;
+        baton->fieldNames.push_back(mysqlField->name);
       }
 
       MYSQL_ROW mysqlRow;
@@ -76,10 +55,9 @@ static void query(uv_work_t* req) {
       while((mysqlRow = mysql_fetch_row(result))) {
         lengths = mysql_fetch_lengths(result);
         Row* row = new Row();
-        row->fieldValues = new char*[mysql_num_fields(result)];
+        row->fieldValues.resize(mysql_num_fields(result));
         for(unsigned int i = 0; i < mysql_num_fields(result); i++) {
-          row->fieldValues[i] = new char[lengths[i]];
-          memcpy ( row->fieldValues[i], mysqlRow[i], lengths[i] );
+          row->fieldValues.push_back(mysqlRow[i]);
         }
         baton->rows[rowCounter] = row;
         rowCounter++;
@@ -93,20 +71,16 @@ static void afterQuery(uv_work_t* req, int bla) {
   QueryBaton* baton = static_cast<QueryBaton*>(req->data);
   Handle<Array> array = Array::New();
 
-  for(int i = 0; i < baton->numRows; i++) {
+  for(unsigned int i = 0; i < baton->rows.size(); i++) {
     Handle<Object> obj = Object::New();
-    for(int j = 0; j < baton->numFields; j++) {
-      obj->Set(String::New(baton->fieldNames[j]), String::New(baton->rows[i]->fieldValues[j]));
+    for(unsigned int j = 0; j < baton->fieldNames.size(); j++) {
+      obj->Set(String::New(baton->fieldNames[j].c_str()),
+        String::New(baton->rows[i]->fieldValues[j].c_str()));
     }
     array->Set(Number::New(i), obj);
   }
 
-  Handle<Value> error;
-  if(baton->errorText != 0) {
-    error = String::New(baton->errorText);
-  } else {
-    error = Undefined();
-  }
+  Handle<Value> error = String::New(baton->errorText.c_str());
   Handle<Value> argv[] = { array, error };
   baton->callback->Call(Context::GetCurrent()->Global(), 2, argv);
   baton->callback.Dispose();
